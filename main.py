b7076d6efd6d7a672ced75f1784a0e0e7d8cba7b
Winterval <stevehanhojin@gmail.com>
2024-03-27 00:40:27 +0900

    feat: main.py에 내용 추가하여 CRUD API 구현

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List, Optional
 
 app = FastAPI()
 
@@ -7,8 +8,41 @@ class Book(BaseModel):
     id: int
     title: str
     author: str
-    description: str
+    description: Optional[str]
     published_year: int
+
+bookDB: List[Book] = []
+
+@app.post("/books", response_model=Book)
+async def createBook(book: Book):
+    bookDB.append(book)
+    return book
+
+@app.get("/books", response_model=List[Book])
+async def printBookList():
+    return bookDB
+
+@app.get("/books/{book_id}", response_model=Book)
+async def printSingleBook(book_id: int):
+    for book in bookDB:
+        if book.id == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Book Not Found")
+
+@app.put("/books/{book_id}", response_model=Book)
+async def updateBook(book_id: int, book: Book):
+    for i, book in enumerate(bookDB):
+        if book.id == book_id:
+            bookDB[i] = book
+            return book
+    raise HTTPException(status_code=404, detail="Book Not Found")
+
+@app.delete("/books/{book_id}")
+async def removeBook(book_id: int):
+    for i, book in enumerate(bookDB):
+        del bookDB[i]
+        return {"message": "Selected Book Deleted!"}
+    raise HTTPException(status_code=404, detail="Book Not Found")
 
 @app.get("/")
 async def returnRootMessage():
