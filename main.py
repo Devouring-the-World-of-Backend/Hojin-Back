cf29a8c3f5697d3c068e4868bde0f99a9383ac92
Winterval <stevehanhojin@gmail.com>
2024-03-27 00:21:48 +0900

    feat: Pydantic 이용하여 'Book' 도서 데이터 모델 생성

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    published_year: int
 
 @app.get("/")
 async def returnRootMessage():
